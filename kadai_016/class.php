98caa43f0871f21e74319c877109da86ed393406
KotaOkada-winas <k-okada@winas.jp>
2025-05-12 18:21:09 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>class.php</title>
+</head>
+<body>
+  <p>
+    <?php
+    class Food {
+      private $name;
+      private $price;
+
+      public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+      }
+      public function show_price() {
+        echo $this->price .'<br>';
+      }
+    }
+    ?>
+  </p>
+  <p>
+    <?php
+    class Animal {
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+      }
+      public function show_height() {
+        echo $this->height .'<br>';
+      }
+    }
+    ?>
+  </p>
+  <p>
+    <?php
+    $Food = new Food('potato',250);
+    print_r($Food);
+    $Animal = new animal('dog', 60, 5000);
+    print_r($Animal);
+
+    $Food->show_price();
+    $Animal->show_height();
+    ?>
+  </p>
+  
+</body>
+</html>
